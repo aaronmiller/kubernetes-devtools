1bbeaabab29d2df602fec275957bf013fdf2bf82
Aaron Miller <amiller719@gmail.com>
2022-10-14 22:52:09 -0400

    removing paths.

--- scripts/index.sh
+++ scripts/index.sh
@@ -3,5 +3,4 @@
 export KUBERNETES_DEVTOOLS_DIR="${DEVTOOLS_DIR}/kubernetes-devtools"
 
 . "${KUBERNETES_DEVTOOLS_DIR}/scripts/environment-variables/index.sh"
-. "${KUBERNETES_DEVTOOLS_DIR}/scripts/paths/index.sh"
 . "${KUBERNETES_DEVTOOLS_DIR}/scripts/aliases/index.sh"
