53a4c63a5677d4e98c42e7d909c01e873a89d409
Aaron Miller <amiller719@gmail.com>
2022-10-14 22:51:27 -0400

    cleaning up kubernetes devtools.

--- scripts/paths/index.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for file in "${KUBERNETES_DEVTOOLS_DIR}"/scripts/paths/*; do
-  if [[ $file == *"index.sh" ]]; then
-    continue;
-  fi
-
-  . $file;
-done
--- scripts/paths/krew.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
