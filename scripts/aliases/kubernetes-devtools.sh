286f44b4f9a94fd87fcacb29fbd5221faa3578e5
Aaron Miller <amiller719@gmail.com>
2022-11-07 20:15:52 -0500

    changing to sh.

--- scripts/aliases/kubernetes-devtools.sh
+++ scripts/aliases/kubernetes-devtools.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 alias kubernetes-devtools="cd \${DEVTOOLS_DIR}/kubernetes-devtools"
--- scripts/environment-variables/k9s.sh
+++ scripts/environment-variables/k9s.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 export K9SCONFIG="${KUBERNETES_DEVTOOLS_DIR}/k9s"
