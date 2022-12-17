416b9c1480ae605a644ea68e42ec350344c4e49d
Aaron Miller <aaron.miller@braze.com>
2022-12-17 16:19:51 -0500

    fixing k8s devtools

--- scripts/index.sh
+++ scripts/index.sh
@@ -2,5 +2,5 @@
 
 export KUBERNETES_DEVTOOLS_DIR="${DEVTOOLS_DIR}/kubernetes-devtools"
 
-. "${KUBERNETES_DEVTOOLS_DIR}/scripts/environment-variables/index.sh"
+. "${KUBERNETES_DEVTOOLS_DIR}/scripts/env/index.sh"
 . "${KUBERNETES_DEVTOOLS_DIR}/scripts/aliases/index.sh"
