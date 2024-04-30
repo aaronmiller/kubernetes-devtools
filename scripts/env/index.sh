be312a04bbc190bf1d1b91e93d157587bb421314
Aaron Miller <aaron.miller@braze.com>
2024-04-30 18:47:04 -0400

    fixing kubernetes-devtools

---
+++ scripts/env/index.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for file in "${KUBERNETES_DEVTOOLS_DIR}"/scripts/env/*; do
+  if [[ $file == *"index.sh" ]]; then
+    continue
+  fi
+
+  . $file
+done
---
+++ scripts/env/k9s.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export K9S_CONFIG_DIR="${KUBERNETES_DEVTOOLS_DIR}/k9s"
